bea2c9a296d433948bb2cf8901f707a8e834cffe
GaraevIM <garaevildarm@gmail.com>
2023-01-23 23:05:41 +0500

    - вывели колхоз на экран

--- app/src/main/java/com/example/weatherfetcher/constants.kt
+++ app/src/main/java/com/example/weatherfetcher/constants.kt
@@ -1,5 +1,5 @@
 package com.example.weatherfetcher
 
 const val API_KEY = "d53f1279195166407711058733b4b7c0"
-const val BASE_URL = "api.openweathermap.org/data/2.5"
+const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
 
